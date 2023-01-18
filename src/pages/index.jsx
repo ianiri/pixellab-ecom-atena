834460b4489292d98549f25f8b0f20e229133631
ianiri <44301798+ianiri@users.noreply.github.com>
2023-01-18 21:52:58 +0200

    18ian - install-setup-start

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,8 @@
-import Head from 'next/head'
+// import {css} from '@emotion/css';
+// import { FaApple } from 'react-icons/fa';
+
+import Head from 'next/head';
+import {Layout} from '../layouts/Layout';
 
 export default function Home() {
   return (
@@ -8,9 +12,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        Hello Atena!
-      </main>
+
+      <Layout>
+        <main>
+          <h1>Heading</h1>
+          <p>project content paraghraph</p>
+        </main>
+      </Layout>
     </>
-  )
+  );
 }
