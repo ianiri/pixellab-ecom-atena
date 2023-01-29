5b12ddacc85186ba9e621d742c5eb2c183e26d0e
ianiri <44301798+ianiri@users.noreply.github.com>
2023-01-29 15:21:39 +0200

    Tema 25 Ian - scrool to top update

--- src/components/common/footer/BackToTop.jsx
+++ src/components/common/footer/BackToTop.jsx
@@ -1,13 +1,18 @@
-import Link from "next/link";
 import { IoIosArrowDropup} from 'react-icons/io';
-
 
 export const BackToTop = () => {
   return (
-    <span className="absolute bg-zinc-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <Link href="#top">
-        <IoIosArrowDropup size="32"></IoIosArrowDropup>
-      </Link>
-    </span>
+    <button
+      type="button"
+      className="absolute bg-zinc-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      onClick={() => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }}>
+      <IoIosArrowDropup size="32"></IoIosArrowDropup>
+    </button>
   )
 }
